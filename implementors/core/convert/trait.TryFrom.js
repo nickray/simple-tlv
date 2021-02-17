d17fe15c254533fcd7ef8580bd30776f4e707fd5
nickray <nickray@users.noreply.github.com>
2021-02-17 20:30:19 +0000

    deploy: 416db86b0d92f6b5b573dd0a35bff2a165a4bfa4

--- implementors/core/convert/trait.TryFrom.js
+++ implementors/core/convert/trait.TryFrom.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["simple_tlv"] = [{"text":"impl TryFrom&lt;usize&gt; for Length","synthetic":false,"types":[]},{"text":"impl TryFrom&lt;u8&gt; for Tag","synthetic":false,"types":[]}];
+implementors["simple_tlv"] = [{"text":"impl&lt;'a&gt; TryFrom&lt;&amp;'a [&amp;'a (dyn Encodable + 'a)]&gt; for Length","synthetic":false,"types":[]},{"text":"impl TryFrom&lt;usize&gt; for Length","synthetic":false,"types":[]},{"text":"impl TryFrom&lt;u8&gt; for Tag","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["simple_tlv"] = {"name":"","files":["decoder.rs","encoder.rs","error.rs","header.rs","length.rs","lib.rs","nested.rs","tag.rs","tagged_slice.rs","traits.rs"]};
+sourcesIndex["simple_tlv"] = {"name":"","files":["decoder.rs","encoder.rs","error.rs","header.rs","length.rs","lib.rs","slice.rs","tag.rs","tagged.rs","traits.rs"]};
 createSourceSidebar();
