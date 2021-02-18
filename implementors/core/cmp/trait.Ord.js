45882acfe6434e7480569b988122a239b26eafa6
nickray <nickray@users.noreply.github.com>
2021-02-18 02:14:27 +0000

    deploy: 30b17190e10e16020cbd3f4c180d11918820f88d

--- implementors/core/cmp/trait.Ord.js
+++ implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,5 @@
 (function() {var implementors = {};
+implementors["proc_macro2"] = [{"text":"impl Ord for Ident","synthetic":false,"types":[]}];
 implementors["simple_tlv"] = [{"text":"impl Ord for Length","synthetic":false,"types":[]}];
+implementors["syn"] = [{"text":"impl Ord for Lifetime","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/cmp/trait.PartialOrd.js
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -1,3 +1,5 @@
 (function() {var implementors = {};
+implementors["proc_macro2"] = [{"text":"impl PartialOrd&lt;Ident&gt; for Ident","synthetic":false,"types":[]}];
 implementors["simple_tlv"] = [{"text":"impl PartialOrd&lt;Length&gt; for Length","synthetic":false,"types":[]}];
+implementors["syn"] = [{"text":"impl PartialOrd&lt;Lifetime&gt; for Lifetime","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
